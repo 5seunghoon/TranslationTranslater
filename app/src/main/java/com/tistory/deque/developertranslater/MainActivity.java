6d89c886f4f39ece21268a2f6c37b1180bad87e1
Oh Seung-hoon <stdsalmon@gmail.com>
2018-03-14 10:12:58 +0900

    Add trasnlate core class and translating function, but it is return error yet

--- app/src/main/java/com/tistory/deque/developertranslater/MainActivity.java
+++ app/src/main/java/com/tistory/deque/developertranslater/MainActivity.java
@@ -2,6 +2,7 @@ package com.tistory.deque.developertranslater;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
   EditText inputEditText;
   Button translateButton;
   TextView translateTextView;
+  String originalString;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +22,16 @@ public class MainActivity extends AppCompatActivity {
     inputEditText = findViewById(R.id.inputEditText);
     translateButton = findViewById(R.id.translateButton);
     translateTextView = findViewById(R.id.translateTextView);
+
+    translateButton.setOnClickListener(new View.OnClickListener(){
+
+      @Override
+      public void onClick(View v) {
+        originalString = inputEditText.getText().toString();
+        String translateString = Translate.translating(originalString);
+        translateTextView.setText(translateString);
+      }
+    });
 
     inputEditText.clearFocus(); //앱을 켰을때 텍스트편집뷰에 포커스가 가면 화면을 가린다. 따라서 포커스를 제거
   }
