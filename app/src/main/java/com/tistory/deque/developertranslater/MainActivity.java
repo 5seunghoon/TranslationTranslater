d4288010bee8e07467e1e57389d7c4254ef6bd23
Oh Seung-hoon <stdsalmon@gmail.com>
2018-03-21 14:28:01 +0900

    Add share method
    If user want to share translated text, user will click share button.
    So if share button will clicked, it can share translated text.
    Based on kakaotalk, it will send "translated text" and "app name"

--- app/src/main/java/com/tistory/deque/developertranslater/MainActivity.java
+++ app/src/main/java/com/tistory/deque/developertranslater/MainActivity.java
@@ -1,12 +1,14 @@
 package com.tistory.deque.developertranslater;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -18,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
   Button translateButton;
   TextView translateTextView;
   String originalString;
+  InputMethodManager imm;
+
+  String AppName;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +32,9 @@ public class MainActivity extends AppCompatActivity {
     inputEditText = findViewById(R.id.inputEditText);
     translateButton = findViewById(R.id.translateButton);
     translateTextView = findViewById(R.id.translateTextView);
+
+    imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+
 
     inputEditText.clearFocus(); //if focus on, keyboard will hide display
   }
@@ -39,6 +47,8 @@ public class MainActivity extends AppCompatActivity {
       NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
       if(networkInfo != null && networkInfo.isConnected()){ //check Internet connection
         new TranslateAsyncTask(translateTextView, "en", "ko").execute(originalString);
+        imm.hideSoftInputFromWindow(inputEditText.getWindowToken(), 0);//if click button, keyboard will hide.
+
         //Android is not connect network main thread
         //So call TranslateAsyncTask which extends AsyncTask. It execute new thread for connecting network.
       }
@@ -49,5 +59,18 @@ public class MainActivity extends AppCompatActivity {
     catch (Exception e){
       Log.d("MAIN_ACTIVITY", e.toString());
     }
+  }
+
+  public void clickShareButton(View view){
+    /**
+     * if click share button and share to kakaotalk, it will send <<Translated Text>> massage.
+     */
+    String translatedString = translateTextView.getText().toString();
+    Intent shareIntent = new Intent();
+    shareIntent.setAction(Intent.ACTION_SEND);
+    shareIntent.setType("text/plan");
+    shareIntent.putExtra(Intent.EXTRA_TITLE, getString(R.string.app_name));
+    shareIntent.putExtra(Intent.EXTRA_TEXT,translatedString + "\n - translated by " + getString(R.string.app_name));
+    startActivity(Intent.createChooser(shareIntent, "번역 결과 공유하기"));
   }
 }
