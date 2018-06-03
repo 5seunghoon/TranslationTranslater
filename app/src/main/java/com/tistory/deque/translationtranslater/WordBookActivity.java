7195a27dda2a74d21d1f50caf395e24a4b08e8e7
hyunmoahn <mos_dev@naver.com>
2018-06-03 23:44:55 +0900

    Make Layout list view using ExcludingMember in Database

--- app/src/main/java/com/tistory/deque/translationtranslater/WordBookActivity.java
+++ app/src/main/java/com/tistory/deque/translationtranslater/WordBookActivity.java
@@ -5,31 +5,47 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.ScrollView;
+
+import java.util.ArrayList;
 
 public class WordBookActivity extends AppCompatActivity {
 
   private DBOpenHelper dbHelper;
   private final int dbVersion = 1;
 
+  ListView wordList;
   EditText wordKey;
   EditText wordValue;
   Button addButton;
+
+  ArrayList<ExcludingMember> WordBookList;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_word_book);
 
+    wordBookDBOpen();
+
     addButton = findViewById(R.id.add_btn);
     wordKey = findViewById(R.id.word_key);
     wordValue = findViewById(R.id.word_value);
 
+    wordList = (ListView)findViewById(R.id.word_list);
+    WordBookList = dbHelper.getWords();
+    WordBookAdapter adapter = new WordBookAdapter(this, WordBookList);
+    wordList.setAdapter(adapter);
+
     addButton.setOnClickListener(new View.OnClickListener() {
       public void onClick(View v) {
         String key = wordKey.getText().toString();
-        String value = wordKey.getText().toString();
+        String value = wordValue.getText().toString();
 
         addWordBook(key, value);
 
@@ -38,7 +54,7 @@ public class WordBookActivity extends AppCompatActivity {
       }
     });
 
-    wordBookDBOpen();
+    dbHelper.getWords();
   }
 
   private void wordBookDBOpen() {
@@ -52,5 +68,7 @@ public class WordBookActivity extends AppCompatActivity {
 
   private void addWordBook(String key, String value) {
     dbHelper.insertWord(key, value);
+    WordBookAdapter adapter = new WordBookAdapter(this, dbHelper.getWords());
+    wordList.setAdapter(adapter);
   }
 }
---
+++ app/src/main/java/com/tistory/deque/translationtranslater/WordBookAdapter.java
@@ -0,0 +1,39 @@
+package com.tistory.deque.translationtranslater;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+/**
+ * Created by hyunmoahn on 2018. 6. 3..
+ */
+
+public class WordBookAdapter extends ArrayAdapter<ExcludingMember> {
+  public WordBookAdapter(Context context, ArrayList<ExcludingMember> list) {
+    super(context, 0, list);
+  }
+
+  @Override
+  public View getView(int position, View convertView, ViewGroup parent) {
+    ExcludingMember member = getItem(position);
+
+    if (convertView == null) {
+      convertView = LayoutInflater.from(getContext()).inflate(R.layout.word_book_item, parent, false);
+    }
+
+    TextView origin = (TextView)convertView.findViewById(R.id.word_book_key);
+    TextView value = (TextView)convertView.findViewById(R.id.word_book_value);
+
+    origin.setText(member.getOrigin());
+    value.setText(member.getValue());
+
+    return convertView;
+  }
+}
